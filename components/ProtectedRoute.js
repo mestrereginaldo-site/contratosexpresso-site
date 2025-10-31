9de7af61950d81cf9bebd3d65cc782ee9ae42170
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-10-31 18:29:44 -0300

    Update ProtectedRoute.js

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -4,22 +4,22 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  const router = useRouter();
+    const { user } = useAuth();
+    const router = useRouter();
 
-  useEffect(() => {
+    useEffect(() => {
+        if (!user) {
+            router.push('/login');
+        }
+    }, [user, router]);
+
     if (!user) {
-      router.push('/login');
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="text-lg">Redirecionando para login...</div>
+            </div>
+        );
     }
-  }, [user, router]);
 
-  if (!user) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-lg">Redirecionando para login...</div>
-      </div>
-    );
-  }
-
-  return children;
+    return children;
 }
