db7919694d578698de158ceef0d46ff0cf68296f
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-01 08:23:04 -0300

    Update page.js

--- app/artigos/[slug]/page.js
+++ app/artigos/[slug]/page.js
@@ -1,5 +1,5 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
 import Link from 'next/link'
 
 // Dados dos 13 artigos - ATUALIZADO COM OS NOVOS SLUGS
